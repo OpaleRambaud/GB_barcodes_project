cc9f7c2b93c667edf770d272c18268ec2368f232
OpaleRambaud <70634291+OpaleRambaud@users.noreply.github.com>
2022-02-07 19:15:13 +0100

    Update phylo.py

--- BSW_programs/program/phylo.py
+++ BSW_programs/program/phylo.py
@@ -1,16 +1,8 @@
 #!/bin/env python3
 # -*- coding: utf-8 -*-
-# This program is free software: you can redistribute it and/or modify it under the terms of the 
-# GNU General Public License as published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version. This program is distributed in the hope that it
-# will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
-# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-# A copy of the GNU General Public License is available at http://www.gnu.org/licenses/gpl-3.0.html
 # Copyright (C) 2022 Alexandre Hassanin MNHN
 __author__ = "Dylan KLEIN"
-__contact__ = "klein.dylan@outlook.com"
-__version__ = "1.0.0"
-__license__ = "GPL"
+
 
 """
 This program is used to get frequencies of bipartition.
